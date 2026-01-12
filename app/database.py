1acf64efa4b289b49686d9ef3c351be724d15c25
Selina Zhang <selinaz@Selinas-MacBook-Pro-2.local>
2026-01-12 15:32:36 +0800

    revised engine

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
