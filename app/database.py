54cb2470825a85b047ed3c235ff98981b25310ab
Selina Zhang <selinaz@Selinas-MacBook-Pro-2.local>
2026-01-14 21:14:46 +0800

    finished adding back button to profile page

--- app/database.py
+++ app/database.py
@@ -1,12 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-engine = create_engine(DATABASE_URL)  # 不用 sslmode，pooler 已处理
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set!")
 
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+# 只 import Base，不 import User/Survey/Response
+from app.models import Base
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
