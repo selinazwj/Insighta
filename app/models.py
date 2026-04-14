033e425b65071b40dd65f9fe629533f0f6606c29
Selina Zhang <selinaz@crc-dot1x-nat-10-239-242-72.bu.edu>
2026-04-13 22:37:25 -0400

    debug json

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, func
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, func, Boolean, JSON
 from sqlalchemy.orm import relationship, declarative_base
 from datetime import datetime
 
